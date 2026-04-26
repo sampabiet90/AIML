34ba767a5736fa2bccbbd61584405639ae616133
Sampa Banerjee <sampabanerjee89@gmail.com>
2026-04-26 22:05:39 +0530

    commit hollow-square-pattern

---
+++ hollow-square-pattern.py
@@ -0,0 +1,15 @@
+# *****
+# *   *
+# *   *
+# *   *
+# *****
+
+N=5
+
+for i in range(1, N+1):
+    for j in range(1,N+1):
+        if i == 1 or i == N or j ==1 or j == N:
+         print('*', end='')
+        else:
+         print(" ", end="")
+    print()
